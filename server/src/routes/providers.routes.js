60cad82105f361cde7057d50a1e20609b0fc6e39
hrishisharma9766 <hrishis@first-insight.com>
2025-11-27 17:51:21 +0530

    mad some folder changes

--- server/src/routes/providers.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { listProviders } from '../controllers/providers.controller.js'
-
-const router = Router()
-
-router.get('/', listProviders)
-
-export default router
